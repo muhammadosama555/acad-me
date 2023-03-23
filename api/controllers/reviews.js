99563a708080084d1557f03554d2f91a8b344ca6
muhammadosama555 <muhammad.osama.31997@gmail.com>
2023-03-23 17:51:58 +0500

    update

--- api/controllers/reviews.js
+++ api/controllers/reviews.js
@@ -12,7 +12,7 @@ const ErrorResponse = require("../utils/errorResponse.js")
 exports.getReviews=asyncHandler(async(req,res,next)=>{
     if(req.params.bootcampId){
         const reviews=await Review.find({bootcamp:req.params.bootcampId})
-    
+      
         res.status(200).json({
             success:true,
             count: reviews.length,
@@ -51,7 +51,7 @@ exports.getReview=asyncHandler(async(req,res,next)=>{
 exports.addReview=asyncHandler(async(req,res,next)=>{
   req.body.bootcamp=req.params.bootcampId
   req.body.user= req.user.id
-  console.log(req.params);
+  
   const bootcamp=await Bootcamp.findById(req.params.bootcampId)
 
   if(!bootcamp){
@@ -59,6 +59,38 @@ exports.addReview=asyncHandler(async(req,res,next)=>{
   }
 
   const review=await Review.create(req.body)
+
+  res.status(201).json({
+    success:true,
+    data: review
+  })
+
+})
+// Update   Reviews
+//route    Put   /api/v1/reviews/:id
+//access   private
+
+exports.updateReview=asyncHandler(async(req,res,next)=>{
+  
+  let review=await Review.findById(req.params.id)
+
+  if(!review){
+    return next(new ErrorResponse('no review found with given id',404))
+  }
+
+  //Make sure review belongs to user
+  if(review.user.toString() !==req.user.id && req.user.role !== 'admin' ){
+
+    return next(new ErrorResponse('Not authorized',401))
+
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id,req.body,{
+    new:true,
+    runValidators:true
+  })
+
+  
 
   res.status(201).json({
     success:true,
--- api/routes/reviews.js
+++ api/routes/reviews.js
@@ -4,11 +4,11 @@ const Review=require('../models/Review')
 const router=express.Router({mergeParams: true})
 
 const {protect, authorize}=require('../middlewares/auth')
-const { getReviews, getReview, addReview } = require('../controllers/reviews')
+const { getReviews, getReview, addReview, updateReview } = require('../controllers/reviews')
 
 router.get('/',getReviews)
 router.post('/',protect,authorize('user','admin'),addReview)
-
+router.put('/:id',protect,authorize('user'),updateReview)
 router.get('/:id',getReview)
 module.exports=router
 
