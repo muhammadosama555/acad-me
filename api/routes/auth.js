6aee7f2a7d9b917650ee30ba1d9837133a208c55
muhammadosama555 <muhammad.osama.31997@gmail.com>
2023-03-23 14:07:23 +0500

    logout

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { registerUser, loginUser, getMe, forgotPassword } = require('../controllers/authController')
+const { registerUser, loginUser, getMe, forgotPassword, logout } = require('../controllers/authController')
 const router=express.Router()
 
 
@@ -7,6 +7,7 @@ const router=express.Router()
 
 router.post('/register',registerUser)
 router.post('/login',loginUser)
+router.get('/logout',logout)
 router.get('/me',getMe)
 router.post('/forgotPassword',forgotPassword)
 
